6b64710a304199790563a947cdd7b7c060eac6da
David <david.brouwer.99@gmail.com>
2017-02-20 10:39:58 +0100

    Worked on the tutorials of Derek Banas and the book.

---
+++ WeatherApp/main.py
@@ -0,0 +1,23 @@
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import *
+
+
+class AddLocationForm(BoxLayout):
+    Height_One = StringProperty('40dp')
+    Button_Width = NumericProperty(25)
+    TextBox_Width = NumericProperty(50)
+
+
+class WeatherApp(App):
+    #
+    pass
+    # def build(self):
+    #     return AddLocationForm()
+
+if __name__ == '__main__':
+    WeatherApp().run()
