56fb9365fc49eba642e10254770842abc3ddfe64
David <david.brouwer.99@gmail.com>
2017-05-30 17:41:34 +0200

    Some changes man

---
+++ Project One/JustAnotherTestFile.py
@@ -0,0 +1,21 @@
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
+
+Builder.load_file("JustAnotherTestFile.kv")
+
+class RootWidget(Widget):
+    two = NumericProperty(200)
+
+
+class MyApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == '__main__':
+    MyApp().run()
