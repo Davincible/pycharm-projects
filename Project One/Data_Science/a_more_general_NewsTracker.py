a6b4c3628e213c1d00b53870fd9773080f6c77d1
David <david.brouwer.99@gmail.com>
2017-07-01 12:06:33 +0200

    Started working to track other sites than nu.nl

--- Project One/Data_Science/a_more_general_NewsTracker.py
+++ Project One/Data_Science/a_more_general_NewsTracker.py
@@ -6,7 +6,9 @@ import bs4 as bs
 from unidecode import unidecode
 import re
 
-url = 'http://www.nu.nl'
+#url = 'http://www.nu.nl'
+url = 'http://www.businesswire.com/portal/site/home/news/'
+website = 'http://www.businesswire.com'
 
 with op(url) as connection:
     raw_data = connection.read()
@@ -15,24 +17,27 @@ soup = bs.BeautifulSoup(raw_data, 'lxml')
 articles = []
 
 ## data is wrapped in li tags, and then packed in a ul tag
-div = soup.find('div', class_='column first')
-article_list = div.ul
 interests = ['trump', "assassin's creed", 'cia', 'eu', 'cameraman', 'amsterdam', 'italië']
 
 
 index = 0
-for article in div.find_all('li'):
-    articles.append({})
-    try:
-        articles[index]['href'] = "http://www.nu.nl{}".format(article.a.get('href'))
-        articles[index]['timestamp'] = article.find('span', class_='timestamp').text
-        articles[index]['timestamp'] = unidecode(articles[index]['timestamp'])
-        articles[index]['section'] = article.find('span', class_='section').text
-        articles[index]['title'] = article.find('span', class_='title').text
-    except AttributeError:
-        pass
+for article_list in soup.find_all('ul'):
+    for article in article_list.find_all('li'):
+        articles.append({})
+        try:
+            link = article.a.get('href')
+            if link[0] == '/':
+                articles[index]['href'] = "{}{}".format(website, link)
+            else:
+                articles[index]['href'] = link
+            articles[index]['timestamp'] = article.find('span', class_='timestamp').text
+            articles[index]['timestamp'] = unidecode(articles[index]['timestamp'])
+            articles[index]['section'] = article.find('span', class_='section').text
+            articles[index]['title'] = article.find('title').text
+        except AttributeError:
+            pass
 
-    index +=1
+        index +=1
 
 
     # try:
@@ -53,3 +58,6 @@ for interest in interests:
 
 print("De hele lijst:", articles)
 print("Heeft een lengte van:", len(articles))
+with open('C:/Users/David.MIDDENAARDE/Documents/general_NewsTracker_Output.txt', 'w') as output_document:
+    for item in articles:
+        output_document.write(str(item) + '\n')
