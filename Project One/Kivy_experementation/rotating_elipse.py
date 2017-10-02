92f8cbad696cddbe3388bdb546254295fea90cb6
David <david.brouwer.99@gmail.com>
2017-10-02 14:52:19 +0200

    Automated the rotating process, learned how to get the widget ids outside the widget class.

--- Project One/Kivy_experementation/rotating_elipse.py
+++ Project One/Kivy_experementation/rotating_elipse.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.clock import Clock
 import math
 
 
@@ -9,12 +10,13 @@ class MyWidget(Widget):
     """My Widget represents a circle and a rotating ellipse
     """
     angle = NumericProperty(0)
-    width_ball = NumericProperty(40)
-    height_ball = NumericProperty(40)
+    width_ball = NumericProperty(400)
+    height_ball = NumericProperty(400)
     ball_size = ReferenceListProperty(width_ball, height_ball)
-    width_ellipse = NumericProperty(100)
-    height_ellipse = NumericProperty(20)
+    width_ellipse = NumericProperty(50)
+    height_ellipse = NumericProperty(50)
     ellipse_size = ReferenceListProperty(width_ellipse, height_ellipse)
+    direction = True
 
 
     def on_touch_down(self, touch):
@@ -25,12 +27,39 @@ class MyWidget(Widget):
         radians = math.atan2(touch.y - self.parent.center_y, touch.x)
         self.angle = math.degrees(radians)
 
+    def move_ellipse(self, *args):
+        print("move called", self.angle)
+
+        # ins = App.get_running_app().window.widget_var
+
+        if self.angle < -90:
+            self.direction = False
+
+        elif self.angle > 90:
+            self.direction = True
+
+        if self.direction == True:
+            self.angle -= 4
+
+        elif self.direction == False:
+            self.angle += 4
+
+
 class MyGame(Widget):
-    pass
+    widget_var = MyWidget()
+
+
 
 class Rotating_ElipseApp(App):
+    # window = MyGame()
+
+
     def build(self):
         window = MyGame()
+        # widget_var = MyWidget()
+        trigger = lambda *args: Clock.schedule_interval(App.get_running_app().root.ids['widget_1'].move_ellipse, 0.0001)
+        Clock.schedule_once(trigger, 1)
+
         return window
 
 if __name__ == '__main__':
