87158f928944ecc20ca1542ac7b113030b602389
David <david.brouwer.99@gmail.com>
2018-03-13 16:34:28 +0100

    something

--- Project One/socket_stuff/secure_server_side.py
+++ Project One/socket_stuff/secure_server_side.py
@@ -24,7 +24,7 @@ def main():
         conn = None
         ssock, addr = sock.accept()
         try:
-            conn = context.wrap_socket(ssock, server_side=True)
+            conn = context.wrap_socket(ssock, certfile=CERT, server_side=True)
             handle(conn)
         except ssl.SSLError as e:
             print(e)
