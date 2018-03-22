bc95a5227a686fda642409846c6e67e4a139c44e
David <david.brouwer.99@gmail.com>
2018-03-22 18:35:13 +0100

    little change

--- Project One/socket_stuff/secure_server_side.py
+++ Project One/socket_stuff/secure_server_side.py
@@ -40,7 +40,7 @@ def process_request(request, conn):
         ## should do some validation shit
         function_call = request['header']['FunctionCall']
         print("the function call is:", function_call)
-        if function_call is 'request_token':
+        if function_call == 'request_token':
             credentials = {request['body']['Data']['username']: request['body']['Data']['password']}
             print("calling token method")
             token = create_token(credentials)
