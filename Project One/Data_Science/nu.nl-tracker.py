6dabb93a43813572e798c29f5030f360b302ee94
David <david.brouwer.99@gmail.com>
2018-04-03 16:04:29 +0200

    use urllib3

--- Project One/Data_Science/nu.nl-tracker.py
+++ Project One/Data_Science/nu.nl-tracker.py
@@ -2,14 +2,34 @@
 ##  Search the whole data base for interests
 ## nu.nl/tag/<interest>
 from urllib.request import urlopen as op
+import urllib3
+import certifi
+from random import randint
+from fake_useragent import UserAgent
 import bs4 as bs
 from unidecode import unidecode
 import re
 
 url = 'http://www.nu.nl'
 
-with op(url) as connection:
-    raw_data = connection.read()
+method = 3
+raw_data = None
+if method is 0:
+    with op(url) as connection:
+        raw_data = connection.read()
+else:
+    print("using urllib3")
+    useragent = UserAgent()
+    header_list = ['firefox', 'internetexplorer', 'safari', 'chrome', 'opera']
+    Headers = {'User-Agent': useragent.data_browsers[header_list[randint(0, 4)]][randint(0, 49)]}
+
+    pm = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    try:
+        connection = pm.request('GET', url, headers=Headers)
+        raw_data = connection.data
+    except:
+        print("Failed to connect.")
+
 
 soup = bs.BeautifulSoup(raw_data, 'lxml')
 articles = []
@@ -17,7 +37,7 @@ articles = []
 ## data is wrapped in li tags, and then packed in a ul tag
 div = soup.find('div', class_='column first')
 article_list = div.ul
-interests = ['trump', "assassin's creed", 'cia', 'eu', 'cameraman', 'amsterdam', 'italië']
+interests = ['trump', "assassin's creed", 'cia', 'eu', 'cameraman', 'amsterdam', 'italië', 'nedeRlandse']
 
 
 index = 0
